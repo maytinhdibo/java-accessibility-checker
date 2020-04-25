ae780f1e8ec39634b557424d5f62547f2688f0fe
maytinhdibo <iammaytinhdibo@gmail.com>
2020-04-25 17:16:59 +0700

    add symbol solver

---
+++ src/main/java/parser/SymbolSolver.java
@@ -0,0 +1,25 @@
+package parser;
+
+import com.github.javaparser.ast.CompilationUnit;
+import com.github.javaparser.ast.expr.Expression;
+import com.github.javaparser.ast.expr.MarkerAnnotationExpr;
+import com.github.javaparser.resolution.types.ResolvedType;
+import utils.Log;
+
+public class SymbolSolver {
+    public static void resolvedType(CompilationUnit cu) {
+        // Find all the calculations with two sides:
+        cu.findAll(Expression.class).forEach(be -> {
+            try {
+                if (!(be instanceof MarkerAnnotationExpr)) {
+                    ResolvedType resolvedType = be.calculateResolvedType();
+
+                    Log.write("\t" + be.toString() + " is a: " + resolvedType);
+                }
+            } catch (Exception e) {
+                System.out.println("NOT SUPPORT: " + be.toString());
+//                e.printStackTrace();
+            }
+        });
+    }
+}
