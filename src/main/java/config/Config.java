f15006c91ee05a2242084c27a8eaa677a10758e6
maytinhdibo <iammaytinhdibo@gmail.com>
2020-06-18 15:35:06 +0700

    optimize src path

--- src/main/java/config/Config.java
+++ src/main/java/config/Config.java
@@ -2,8 +2,10 @@ package config;
 
 public class Config {
     public static String STORAGE_DIR = "/Users/maytinhdibo/Project/java-accessibility-checker/storage/";
-    public static String PROJECT_DIR = "/Users/maytinhdibo/Downloads/data/cassandra/";
-    public static String[] SOURCE_PATH = {"/Users/maytinhdibo/Downloads/data/cassandra/"};
+
+    public static String PROJECT_DIR = "/Users/maytinhdibo/Downloads/data/cassandra/src/";
+    public static String[] SOURCE_PATH = {"/Users/maytinhdibo/Downloads/data/cassandra/src/"};
+
     public static String[] CLASS_PATH = {
             "/Users/maytinhdibo/Downloads/data/cassandra/lib/antlr-3.2.jar",
             "/Users/maytinhdibo/Downloads/data/cassandra/lib/commons-cli-1.1.jar"
