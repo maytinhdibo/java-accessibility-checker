8162e701ec59fcb7371ebf80cf40be61436fe5a5
maytinhdibo <iammaytinhdibo@gmail.com>
2020-07-15 10:09:33 +0700

    demo file test

--- src/main/java/config/Config.java
+++ src/main/java/config/Config.java
@@ -3,13 +3,16 @@ package config;
 public class Config {
     public static String STORAGE_DIR = "/Users/maytinhdibo/Project/java-accessibility-checker/storage/";
 
-    public static String PROJECT_DIR = "/Users/maytinhdibo/Downloads/data/cassandra/src/";
-    public static String[] SOURCE_PATH = {"/Users/maytinhdibo/Downloads/data/cassandra/src/"};
+    public static String PROJECT_DIR = "/Users/maytinhdibo/Project/bomberman/";
+    public static String[] SOURCE_PATH = {"/Users/maytinhdibo/Project/bomberman/src/"};
 
     public static String[] CLASS_PATH = {
-            "/Users/maytinhdibo/Downloads/data/cassandra/lib/antlr-3.2.jar",
-            "/Users/maytinhdibo/Downloads/data/cassandra/lib/commons-cli-1.1.jar"
+//            "/Users/maytinhdibo/Downloads/data/cassandra/lib/antlr-3.2.jar",
+//            "/Users/maytinhdibo/Downloads/data/cassandra/lib/commons-cli-1.1.jar"
     };
+
+    public static String TEST_FILE_PATH = "/Users/maytinhdibo/Project/bomberman/src/com/carlosflorencio/bomberman/TestExtended.java";
+    public static int TEST_POSITION = 1062;
 
     public static String LOG_DIR = "/Users/maytinhdibo/Project/java-accessibility-checker/storage/output/";
 }
